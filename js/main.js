cfd3c361fa2f1b391db8b04ebacdb8cc7f85c22b
Gil-AM <gilbertoandreattamaia@gmail.com>
2021-01-23 16:29:54 -0300

    aula concluída

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 
 
+
+
 function clicou(){
     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
     //console.log(document.getElementById("agradecimento"));
 }
+
+
+
 
 function redirecionar(){
     //window.open("https://www.linkedin.com/in/gilbertoandreatta/");
